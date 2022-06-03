e5d6bbbce5bc57e69eb1099eb5021f5c14e44173
Max <asd@yandex.ru>
2022-06-03 20:16:51 +0300

    Init commit

---
+++ src/main/java/ru/laverno/HelloShell.java
@@ -0,0 +1,22 @@
+package ru.laverno;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class HelloShell {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloShell(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @ShellMethod(key = "hello-to", value = "Say hello to username")
+    public String helloTo(@ShellOption({"username"}) String username) {
+        return helloService.helloTo(username);
+    }
+}
---
+++ src/main/java/ru/laverno/SpringApplicationLesson.java
@@ -0,0 +1,13 @@
+package ru.laverno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationLesson {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationLesson.class, args);
+    }
+
+}
